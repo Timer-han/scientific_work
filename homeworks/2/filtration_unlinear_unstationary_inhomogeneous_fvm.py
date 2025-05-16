FIX: add Kr and S calculation

homeworks/2/filtration_unlinear_unstationary_inhomogeneous_fvm.py
@@ -4,6 +4,43 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
 import sys
 
+# # Пример 2. Celia
+# def get_h(x, t):
+#     return x * 0 + 1
+# def get_f(x, t):
+#     return 2 * np.sin(x) * np.cos(x) - np.cos(x) + np.sin(x) * np.sin(t)
+# def get_k(x):
+#     return 9.22e-3
+# def get_s(x):
+#     return np.sin(x)
+# def get_h_initial(x):
+#     return get_h(x, 0)
+# def get_h_boundaries(t, length):
+#     return get_h(0, t), get_h(length, t)
+# def get_S(h):
+#     return get_ө(h) / get_phi() 
+# def get_Kr(h):
+#     ө_s = 0.368
+#     ө_r = 0.102
+#     n = 2
+#     m = 1 - 1 / n
+#     S_l = (h - ө_r) / (ө_s - ө_r)
+#     return S_l ** 0.5 * (1 - (1 - S_l ** (1 / m)) ** m) ** 2
+# def get_ө(h):
+#     ө_s = 0.368
+#     ө_r = 0.102
+#     a_vg = 0.0335
+#     n = 2
+#     m = 1 - 1 / n
+#     if h < 0:
+#         return ө_r + (ө_s - ө_r) / (1 + abs(a_vg * h) ** n) ** m
+#     else:
+#         return ө_s
+# def get_phi():
+#     return 0.5
+
+
+
 # Пример 1. Всё хорошо
 def get_h(x, t):
     return np.cos(x) + x - np.cos(t)
@@ -22,34 +59,6 @@ def get_S(h):
 def get_Kr(h):
     return 1
 
-# # Пример 2. Решение очень красиво расходится
-# def get_h(x, t):
-#     return np.sin(x) + x - np.cos(t)
-# def get_f(x, t):
-#     return np.sin(x) + (x - 0.9) * np.sin(t)
-# def get_k(x):
-#     return 1
-# def get_s(x):
-#     return x - 0.9
-# def get_h_initial(x):
-#     return get_h(x, 0)
-# def get_h_boundaries(t, length):
-#     return get_h(0, t), get_h(length, t)
-
-# # Пример 3. k = 1, s = 1
-# def get_h(x, t):
-#     return np.sin(x) + x - np.cos(t)
-# def get_f(x, t):
-#     return np.sin(x) + np.sin(t)
-# def get_k(x):
-#     return 1
-# def get_s(x):
-#     return 1
-# def get_h_initial(x):
-#     return get_h(x, 0)
-# def get_h_boundaries(t, length):
-#     return get_h(0, t), get_h(length, t)
-
 
 def get_tridiagonal_matrix(n: int):
     matrix = np.zeros((n, n))
@@ -65,48 +74,56 @@ def get_tridiagonal_matrix(n: int):
 
 def get_diagonal(n, cell_size, time_interval, h_init):
     diagonal = np.full(n - 1, 0.)
-    S = get_S(h_init)
     
     for i in range(1, n - 2):
         a = get_k((i - 0.5) * cell_size)
         b = get_k((i + 0.5) * cell_size)
         c = get_k((i + 1.5) * cell_size)
         s = get_s((i + 0.5) * cell_size)
-        Kr = get_Kr(h_init)
-        diagonal[i] = (c * (a + b) + a * (b + c)) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * Kr * b * time_interval)
+        Kr_r = get_Kr(max(h_init[i], h_init[i + 1]))
+        Kr_l = get_Kr(max(h_init[i - 1], h_init[i]))
+        S  = get_S(h_init[i])
+        diagonal[i] = (c * (a + b) * Kr_r + a * (b + c) * Kr_l) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * b * time_interval)
 
     a = get_k(0)
     b = get_k(0.5 * cell_size)
     c = get_k(1.5 * cell_size)
     s = get_s(0.5 * cell_size)
-    Kr = get_Kr(h_init)
+    Kr_l = get_Kr(h_init[0])
+    Kr_r = get_Kr(max(h_init[0], h_init[1]))
+    S = get_S(h_init[0])
 
-    diagonal[0] = (c * (a + b) + 2 * a * (b + c)) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * Kr * b * time_interval)
+    diagonal[0] = (c * (a + b) * Kr_r + 2 * a * (b + c) * Kr_l) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * b * time_interval)
+
 
     a = get_k((n - 2.5) * cell_size)
     b = get_k((n - 1.5) * cell_size)
     c = get_k((n - 1.0) * cell_size)
     s = get_s((n - 1.5) * cell_size)
-    Kr = get_Kr(h_init)
+    Kr_l = get_Kr(max(h_init[-2], h_init[-1]))
+    Kr_r = get_Kr(h_init[-1])
+    S = get_S(h_init[-1])
 
-    diagonal[-1] = (2 * c * (a + b) + a * (b + c)) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * Kr * b * time_interval)
+    diagonal[-1] = (2 * c * (a + b) * Kr_r + a * (b + c) * Kr_l) / ((a + b) * (b + c)) + s * S * cell_size ** 2 / (2 * b * time_interval)
 
     return diagonal
 
-def get_upper_diagonal(n):
+def get_upper_diagonal(n, h_init):
     upper_diagonal = np.full(n - 2, 0.)
     for i in range(0, n - 2):
         a = get_k((i + 0.5) * cell_size)
         b = get_k((i + 1.5) * cell_size)
-        upper_diagonal[i] = - b / (a + b)
+        Kr = get_Kr(max(h_init[i], h_init[i + 1]))
+        upper_diagonal[i] = - b / (a + b) * Kr
     return upper_diagonal
 
-def get_lower_diagonal(n):
+def get_lower_diagonal(n, h_init):
     lower_diagonal = np.full(n - 2, 0.)
     for i in range(0, n - 2):
         a = get_k((i + 0.5) * cell_size)
         b = get_k((i + 1.5) * cell_size)
-        lower_diagonal[i] = - a / (a + b)
+        Kr = get_Kr(max(h_init[i], h_init[i + 1]))
+        lower_diagonal[i] = - a / (a + b) * Kr
     return lower_diagonal
 
 def solve_by_finding_inversed_matrix(matrix, vector):
@@ -154,14 +171,14 @@ def get_norm(vector):
 
 def build_rhs_vector(n, cell_size, time_interval, time_step, h_prev, h_init):
     vector = np.zeros(n - 1)
-    S = get_S(h_prev)
 
     for i in range(0, n - 1):
         x = (i + 0.5) * cell_size
         t = time_step * time_interval
         s = get_s(x)
         k = get_k(x)
-        vector[i] = get_f(x, t) * cell_size ** 2 / (2 * k) + s * cell_size ** 2 / (2 * k * time_interval) * h_prev[i]
+        S = get_S(h_init[i])
+        vector[i] = get_f(x, t) * cell_size ** 2 / (2 * k) + s * S * cell_size ** 2 / (2 * k * time_interval) * h_prev[i]
 
     lengh = n * cell_size
     h_left, h_right = get_h_boundaries(time_step * time_interval, lengh)
@@ -196,8 +213,8 @@ for time_step in range(1, 300):
     converged = False
     for i in range(50):
         diagonal = get_diagonal(n, cell_size, time_interval, h_init)
-        upper_diagonal = get_upper_diagonal(n)
-        lower_diagonal = get_lower_diagonal(n)
+        upper_diagonal = get_upper_diagonal(n, h_init)
+        lower_diagonal = get_lower_diagonal(n, h_init)
 
 
         # vector = build_rhs_vector(n, cell_size, a, b)
@@ -249,13 +266,6 @@ ax.set_ylabel("Solution")
 ax.set_title("Solution of the system")
 ax.legend(loc='upper left')
 ax.grid(True)
-
-
-
-
-
-
-
 
 
 